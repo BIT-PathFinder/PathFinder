ffc980b5fc8542dc51e28420cfb88c3542726cb3
9m1i9n1 <9m1i9n1@gmail.com>
2019-10-22 17:23:19 +0900

    modify sh

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mainaner="9m1i9n1"
+app="pathfinder"
+
+container = `docker ps -a -q --no-trunc --filter name=^/$app$`
+image=`docker images -q $maintaner/$app`
+script_path=$(dirname $(realpath $0))
+
+if [-n "$container"] || ["$container" -eq "1"] ;
+then docker rm -f "$container"
+fi
+
+if [-n "$image"] || ["$image" -eq 1]; 
+then docker rmi "$image"
+fi
+
+docker build -t "$maintaner"/"$app" "$script_path"
+docker run -p 8080:8080 -ㅇ --name "$app" "$maintaner"/"$app"
