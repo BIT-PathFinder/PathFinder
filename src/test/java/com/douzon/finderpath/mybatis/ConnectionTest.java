076f2f28b5e708afa04fb3147a5d9e277004405e
LDH <ehd8266@gmail.com>
2019-10-22 17:58:11 +0900

    MyBatis Connection

---
+++ src/test/java/com/douzon/finderpath/mybatis/ConnectionTest.java
@@ -0,0 +1,40 @@
+package com.douzon.finderpath.mybatis;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class ConnectionTest {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testFactory() {
+		System.out.println(sqlSessionFactory);
+	}
+	
+	@Test
+	public void testConnection() throws Exception {
+		try (Connection conn = dataSource.getConnection()) {
+			System.out.println(conn);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
