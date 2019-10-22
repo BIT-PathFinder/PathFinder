828eaa8e36fefdadc02d967be2ae2eb18841c324
min.dev <9m1i9n1@gmail.com>
2019-10-22 18:12:43 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,14 @@ container = `docker ps -a -q --no-trunc --filter name=^/$app$`
 image=`docker images -q $maintaner/$app`
 script_path=$(dirname $(realpath $0))
 
-if [ -n "$container" ] || [ "$container" -eq "1" ] ;
-then docker rm -f "$container"
+if [ -z "$container"];
+then
+	echo "해당 컨테이너가 존재하지 않아 실행------------------"
+elif [ -n "$container" ] || [ "$container" -eq "1" ] ;
+then 
+	echo "해당 컨테이너가 존재하여 정지시키고 삭제------------"
+	docker stop "$container"
+	docker rm "$container"
 fi
 
 if [ -n "$image" ] || [ "$image" -eq 1 ]; 
