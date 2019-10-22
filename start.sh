5392e3c3e422c76b5e8e8c74917f1b9e2c3aa8d7
9m1i9n1 <9m1i9n1@gmail.com>
2019-10-22 17:30:39 +0900

    modify start.sh

--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ container = `docker ps -a -q --no-trunc --filter name=^/$app$`
 image=`docker images -q $maintaner/$app`
 script_path=$(dirname $(realpath $0))
 
-if [-n "$container"] || ["$container" -eq "1"] ;
+if [ -n "$container" ] || [ "$container" -eq "1" ] ;
 then docker rm -f "$container"
 fi
 
-if [-n "$image"] || ["$image" -eq 1]; 
+if [ -n "$image" ] || [ "$image" -eq 1 ]; 
 then docker rmi "$image"
 fi
 
 docker build -t "$maintaner"/"$app" "$script_path"
-docker run -p 8080:8080 -ㅇ --name "$app" "$maintaner"/"$app"
+docker run -p 8080:8080 -d --name "$app" "$maintaner"/"$app"
